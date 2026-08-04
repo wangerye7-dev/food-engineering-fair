44fb5a34ce1025c8e5b4797d2a2fe56b10caf710
wangerye7-dev <wangerye7@gmail.com>
2026-08-04 17:20:55 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='food-expo-v3-20260804';
+const CACHE='food-expo-v3-1-20260804';
 const FILES=['./','index.html','challenge.html','survey.html','survey-success.html','majors.html','cooperation.html','privacy.html','assets/styles.css','assets/common.js','assets/survey.js','assets/challenge.js','assets/question-bank.js','assets/cooperation.js','assets/school-logo.png','manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
